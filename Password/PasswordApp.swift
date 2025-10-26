337e52fe8e207e9a6cbe67ae5a8f93c33d5d6d2f
Rodrigo Galeano <rodrigo1galeano@gmail.com>
2025-10-26 13:42:17 -0400

    Initial Commit

---
+++ Password/PasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordApp.swift
+//  Password
+//
+//  Created by Rodrigo Galeano on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
